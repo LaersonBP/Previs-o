caa8fd29c96fac9aab431d87abda19644046ca8e
LaersonBP <sonicovortex@gmail.com>
2025-03-03 13:09:00 -0300

    index.html,pagina2.html,script.js,aparencia.css,meu site.js,stylepag3.css

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let pingu = document.getElementById('pame')
+
+window.onload = function() {
+    var imagem = document.getElementById('pame');
+    imagem.classList.add('animarPulo');
+}
+
+document.getElementById("bot").addEventListener("click", function() {
+    window.location.href = 'pagina2.html'
+});
